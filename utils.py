db2de484ef51637c614a74fdfcec0386a462840a
Tahseen Rabbani <trabbani@umd.edu>
2025-01-25 00:06:48 -0500

    update date converter

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 
-def convert_time(filename):
-  #Grab wayback stamp from filename
-  waystamp = ''.join(filter(str.isdigit, filename))
+def convert_time(waystamp):
 
+  #Grab wayback stamp from filename
   #Wayback time format YYYYMMDDhhmmss
   date_object = datetime.strptime(waystamp, "%Y%m%d%H%M%S")
   convert_date = date_object.strftime("%Y-%m-%d")
